94e5f2729ae3796238200ca5ec28597a0d43cfd5
Cherry <yiminxuezheng@gmail.com>
2020-05-17 14:13:11 -0400

    Initial commit

---
+++ MagicCircleNetwork/ArboretumSrc/Arboretum.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+
+namespace MagicCircleNetwork.Arboretum
+{
+    class Arboretum
+    {
+        IModHelper helper;
+        IMonitor monitor;
+        public Arboretum(IModHelper helper, IMonitor monitor)
+        {
+            this.helper = helper;
+            this.monitor = monitor;
+        }
+
+    }
+}
---
+++ MagicCircleNetwork/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace MagicCircleNetwork
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            //Add initialization of each part of the code here. Pass in helper and monitor
+            //that should be the extent of code in this class
+
+            new Arboretum.Arboretum(helper, Monitor);
+        }
+    }
+}
